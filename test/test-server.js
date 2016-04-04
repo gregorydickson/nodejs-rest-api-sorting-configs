603a75b8a55fbc483f8b5ce4a01d401a4d866f61
Gregg Dickson <gregory.d.dickson@gmail.com>
2016-04-04 16:11:14 -0500

    sorting

--- test/test-server.js
+++ test/test-server.js
@@ -13,7 +13,7 @@ describe('configs', function() {
   it('should return an id on /config POST', function(done) {
   chai.request('http://localhost:8080')
     .post('/config')
-    .send({"name": "host1", "hostname": "nessus-ntp.lab.com", "port":1241,"username":"toto"})
+    .send({"name": "host5", "hostname": "nessus-ntp.lab.com", "port":9900,"username":"toto"})
     .end(function(err, res){
       res.should.have.status(200);
       res.should.be.json;
@@ -27,7 +27,7 @@ describe('configs', function() {
   it('should return an id on /config POST', function(done) {
   chai.request('http://localhost:8080')
     .post('/config')
-    .send({"name": "host2", "hostname": "nessus-xml.lab.com", "port":3384,"username":"admin"})
+    .send({"name": "host1", "hostname": "nessus-xml.lab.com", "port":3384,"username":"admin"})
     .end(function(err, res){
       res.should.have.status(200);
       res.should.be.json;
@@ -37,60 +37,101 @@ describe('configs', function() {
       done();
     });
   });
-  
-  it('should respond to /config GET 1', function(done) {
-    chai.request('http://localhost:8080')
-      .get('/config/1')
-      .end(function(err, res){
-          res.should.have.status(200);
-          res.should.be.json;
-          res.body.should.be.a('object');
-          expect(res.body).to.have.property('id');
-          done();
-      });
+
+  it('should return an id on /config POST', function(done) {
+  chai.request('http://localhost:8080')
+    .post('/config')
+    .send({"name": "host4", "hostname": "hosty-thing2.somewhere.com", "port":4757,"username":"testy"})
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      expect(res.body).to.have.property('id');
+
+      done();
+    });
   });
 
-  it('should respond to /config GET 2', function(done) {
-    chai.request('http://localhost:8080')
-      .get('/config/2')
-      .end(function(err, res){
-          res.should.have.status(200);
-          res.should.be.json;
-          res.body.should.be.a('object');
-          expect(res.body).to.have.property('id');
-          done();
-      });
+  it('should return an id on /config POST', function(done) {
+  chai.request('http://localhost:8080')
+    .post('/config')
+    .send({"name": "host3", "hostname": "hosty-thing.somewhere.com", "port":4555,"username":"useitorloseit"})
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      expect(res.body).to.have.property('id');
+
+      done();
+    });
   });
 
-  it('should list configs on /config with no parameter', function(done) {
+  it('should return an id on /config POST', function(done) {
+  chai.request('http://localhost:8080')
+    .post('/config')
+    .send({"name": "host2", "hostname": "thing-hosty.aplace.com", "port":3939,"username":"placestogo"})
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      expect(res.body).to.have.property('id');
+
+      done();
+    });
+  });
+
+  it('should sort configs on /config?sort=name', function(done) {
     chai.request('http://localhost:8080')
-      .get('/config')
+      .get('/config?sort=name')
       .end(function(err, res){
           res.should.have.status(200);
           res.should.be.json;
           res.body.should.be.a('object');
           expect(res.body).to.have.property('configs');
+          res.body.configs[0].name.should.equal('host1');
           done();
       });
   });
 
-  it('should not respond to /config GET when no document', function(done) {
+  it('should sort configs on /config?sort=hostname', function(done) {
     chai.request('http://localhost:8080')
-      .get('/config/3')
-      .end(function(err, res){
-          res.should.have.status(404);
-          done();
-      });
-  });
-
-  it('should delete /config DELETE', function(done) {
-    chai.request('http://localhost:8080')
-      .delete('/config/2')
+      .get('/config?sort=hostname')
       .end(function(err, res){
           res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          expect(res.body).to.have.property('configs');
+          res.body.configs[0].hostname.should.equal('hosty-thing.somewhere.com');
           done();
       });
   });
+
+  it('should sort configs on /config?sort=port', function(done) {
+    chai.request('http://localhost:8080')
+      .get('/config?sort=port')
+      .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          expect(res.body).to.have.property('configs');
+          res.body.configs[0].port.should.equal(3384);
+          done();
+      });
+  });
+  
+  it('should sort configs on /config?sort=username', function(done) {
+    chai.request('http://localhost:8080')
+      .get('/config?sort=username')
+      .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          expect(res.body).to.have.property('configs');
+          res.body.configs[0].username.should.equal("admin");
+          done();
+      });
+  });
+  
 
   
 });
